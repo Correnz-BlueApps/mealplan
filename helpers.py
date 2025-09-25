39eb3565495897fc93f20084c010d95113051bce
Correnz-BlueApps <142593366+Correnz-BlueApps@users.noreply.github.com>
2025-09-25 15:15:23 +0000

    Clean Up recipeById return

--- helpers.py
+++ helpers.py
@@ -7,7 +7,16 @@ def recipeById(recipe_id):
         res = requests.get(url)
         res.raise_for_status()
         data = res.json()
-        return data
+        
+        return {
+            "title": data.get("title"),
+            "rating": data.get("rating"),
+            "hasImage": data.get("hasImage"),
+            "prewiewImageUrlTemplate": data.get("prewiewImageUrlTemplate"),
+            "fullTags": data.get("fullTags"),
+            "ingredients": data.get("ingredientGroups")[0].get("ingredients"),
+            "siteUrl": data.get("siteUrl"),
+        }
 
     except requests.RequestException as e:
         print(f"Request Error: {e}")
